94b319678a737f48c93b92d1512aca8c410dd48a
Lucas Soares Granja <lucassoaresgranja@yahoo.com.br>
2022-08-07 21:52:53 -0300

    Final

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -63,7 +63,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1533900298318-6b8da08a523e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
             name,
             email,
             whatsapp,
